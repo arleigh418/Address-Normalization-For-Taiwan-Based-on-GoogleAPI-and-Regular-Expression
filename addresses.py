8b3010e3e6f282ae2815ab61d7e1020f8f702c75
張峮瑋(Arleigh Chang) <40189046+arleigh418@users.noreply.github.com>
2019-08-12 21:31:38 +0800

    Update addresses.py

--- addresses.py
+++ addresses.py
@@ -44,19 +44,19 @@ def get_api(api_key , addresses):
         others_handle = re.compile(r'\d+樓')
         others_handle2 = re.compile(r'之+\d')
         
-        url = 'https://maps.googleapis.com/maps/api/geocode/json?address='+query+'&language=zh-tw&key='+api_key
+        url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + query + 'sensor=false&language=zh-tw&key=' + api_key
         r = requests.get(url,verify = False)
         data = r.json()
         try:
             if others_handle.search(query) != None:
-                if len(query)+3 - len(data['results'][0]['formatted_address']+'樓')  > 1:
+                if len(query) - len(data['results'][0]['formatted_address']+'樓')  > 1:
                     transformed.append(data['results'][0]['formatted_address']+'樓')
                     print('地址:',query,'， 疑似異常，請檢查')
                 else:
                     transformed.append(data['results'][0]['formatted_address']+'樓')
 
             elif others_handle2.search(query) != None:
-                if  len(query)+3 - len(data['results'][0]['formatted_address']+query[query.index(others_handle.search(query).group()):]) > 1:
+                if  len(query) - len(data['results'][0]['formatted_address']+query[query.index(others_handle.search(query).group()):]) > 1:
                     transformed.append(data['results'][0]['formatted_address']+query[query.index(others_handle.search(query).group()):])
                     print('地址:',query,'， 疑似異常，請檢查')
                 else:
@@ -64,7 +64,7 @@ def get_api(api_key , addresses):
                 
 
             else:
-                if  len(query)+3 - len(data['results'][0]['formatted_address'])   > 1:
+                if  len(query) - len(data['results'][0]['formatted_address'])   > 1:
                     transformed.append(data['results'][0]['formatted_address'])
                     print('地址:',query,'， 疑似異常，請檢查')
                 else:
@@ -172,6 +172,7 @@ def ch_to_num(target_ch):
 def deal_address(address):
     print('---------------修正地址開始---------------')
     after_deal = []
+    error_code = 0
     for i in address:
         i = strQ2B(i)
         i = i.replace(' ','')
@@ -191,64 +192,81 @@ def deal_address(address):
             if regex1.search(i) != None:
                 after_deal.append(re.sub(regex1.search(i).group()[:-1],num_to_ch(regex1.search(i).group()[:-1]),i))
                 print('修正地址 :',i,'| 修正部位:',regex1.search(i).group())
+                error_code+=1
 
             elif  regex1_2.search(i) != None:
                 after_deal.append(re.sub(re.search('\d',regex1_2.search(i).group()).group(),num_to_ch(re.search('\d',regex1_2.search(i).group()).group()),i))
                 print('修正地址 :',i,'| 修正部位:',re.search('\d',regex1_2.search(i).group()).group())
+                error_code+=1
             
             elif  regex2.search(i) != None:
                 after_deal.append(re.sub(regex2.search(i).group()[:-1],num_to_ch(regex2.search(i).group()[:-1]),i))
                 print('修正地址 :',i,'| 修正部位:',regex2.search(i).group())
+                error_code+=1
 
             elif  regex2_2.search(i) != None:
                 after_deal.append(re.sub(re.search('\d',regex2_2.search(i).group()).group(),num_to_ch(re.search('\d',regex2_2.search(i).group()).group()),i))
                 print('修正地址 :',i,'| 修正部位:',re.search('\d',regex2_2.search(i).group()).group())
+                error_code+=1
                 
             elif regex3.search(i) != None:
                 after_deal.append(re.sub(regex3.search(i).group()[:-1],num_to_ch(regex3.search(i).group()[:-1]),i))
                 print('修正地址 :',i,'| 修正部位:',regex3.search(i).group())
+                error_code+=1
 
             elif regex3_2.search(i) != None:
                 after_deal.append(re.sub(re.search('\d',regex3_2.search(i).group()).group(),num_to_ch(re.search('\d',regex3_2.search(i).group()).group()),i))
                 print('修正地址 :',i,'| 修正部位:',re.search('\d',regex3_2.search(i).group()).group())
+                error_code+=1
 
             elif regex4.search(i) != None:
                 after_deal.append(re.sub(regex4.search(i).group()[:-1],ch_to_num(regex4.search(i).group()[:-1]),i))
                 print('修正地址 :',i,'| 修正部位:',regex4.search(i).group())
+                error_code+=1
+
             elif regex5.search(i) != None:
                 after_deal.append(re.sub(regex5.search(i).group()[:-1],ch_to_num(regex5.search(i).group()[:-1]),i))
                 print('修正地址 :',i,'| 修正部位:',regex5.search(i).group())
+                error_code+=1
+
             elif regex6.search(i) != None:
                 after_deal.append(re.sub(regex6.search(i).group()[:-1],ch_to_num(regex6.search(i).group()[:-1]),i))
                 print('修正地址 :',i,'| 修正部位:',regex6.search(i).group())
+                error_code+=1
+
             elif regex7.search(i) != None:
                 after_deal.append(re.sub(regex7.search(i).group()[:-1],num_to_ch(regex7.search(i).group()[:-1]),i))
                 print('修正地址 :',i,'| 修正部位:',regex7.search(i).group())
+                error_code+=1
+
             elif regex8.search(i) != None:
                 after_deal.append(re.sub(regex8.search(i).group()[:-1],ch_to_num(regex8.search(i).group()[:-1]),i))
                 print('修正地址 :',i,'| 修正部位:',regex8.search(i).group())
+                error_code+=1
 
             else:
                 after_deal.append(i)
                 
                 continue
-        except:
+        except IndexError:
             print('修正地址 :',i,'邏輯有誤，未修正')
             after_deal.append(i)
             continue
-    return after_deal
+    return after_deal , error_code
 
        
 
 address = addresses_from_csv(path='test2.xlsx')
 c = 0
-for i in range(3):
+for i in range(0,100):
     if c == 0:
-        addresses = deal_address(address)
+        addresses,error_code = deal_address(address)
+        c+=1
+    elif c !=0 and error_code !=0:
+        addresses , error_code = deal_address(addresses)
         c+=1
     else:
-        addresses = deal_address(addresses)
-        c+=1
+        break
     
 
 
